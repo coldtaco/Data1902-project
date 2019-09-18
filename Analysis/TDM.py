354f8cf0843fd0a4ee5ab67636f77f69c2186d42
coldtaco <coldtaco1@hotmail.comgit config --global user.email yuhang124@hotmail.com>
2019-09-19 06:33:04 +1000

    added TDM and its csv

--- Analysis/TDM.py
+++ Analysis/TDM.py
@@ -7,7 +7,6 @@ from selenium import webdriver
 import traceback
 from sklearn.feature_extraction.text import CountVectorizer
 df = pd.DataFrame()
-docs = [['why hello there', 'omg hello pony', 'she went there? omg why why'],['why hello there', 'omg hello pony', 'she went there? omg why why']]
 mypath = "./Text"
 folders = [join(mypath,f) for f in listdir(mypath) if not isfile(join(mypath, f))]
 folders = [f for f in folders if f != ".git"]
@@ -15,16 +14,20 @@ speeches = [join(g,f) for g in folders for f in listdir(g)  if isfile(join(g, f)
 i = 0
 print(speeches)
 for _file in speeches:
-    print(_file)
-    opened = open(_file,'r', encoding="utf8").readlines()
-    labels = opened.pop(0)
-    txt = [" ".join(opened),]
-    vec = CountVectorizer()
-    X = vec.fit_transform(txt)
-    df_ = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
-    print(labels.split("|"))
-    df_['name'],df_['title'],df_['date'] = labels.split("|")
-    df = df.append(df_).fillna(0)
+    try:
+        print(_file)
+        opened = open(_file,'r', encoding="utf8").readlines()
+        labels = opened.pop(0)
+        txt = [" ".join(opened),]
+        vec = CountVectorizer()
+        X = vec.fit_transform(txt)
+        df_ = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
+        print(labels.split("|"))
+        labs = labels.split("|")
+        df_['name'],df_['title'],df_['date'] = labs[0],labs[1],labs[2]
+        df = df.append(df_).fillna(0)
+    except:
+        continue
 print(df)
 test = open('test.csv','w')
 test.write(df.to_csv(index=False))
