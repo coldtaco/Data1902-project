47f8e20fda03539753f0bd31693e98504d03b2ca
coldtaco <coldtaco1@hotmail.comgit config --global user.email yuhang124@hotmail.com>
2019-09-29 15:40:07 +1000

    Fixed some issues with encoding

--- Text/filterSpeeches.py
+++ Text/filterSpeeches.py
@@ -4,19 +4,22 @@ from os.path import isfile, join
 import re
 from selenium import webdriver
 import traceback
+#removes anything within speeches that are inside brackets
 mypath = "./"
 folders = [f for f in listdir(mypath) if not isfile(join(mypath, f))]
 folders = [f for f in folders if f != ".git"]
 speeches = [join(g,f) for g in folders for f in listdir(g)  if isfile(join(g, f))]
 for _file in speeches:
     print(_file)
-    txt = open(_file,'r').readlines()
+    txt = open(_file,'r',encoding='UTF-8').readlines()
     temp = []
     for line in txt:
         temp.append(re.sub("[\(\[].*?[\)\]]", "", line))
-    with open(_file,'w') as replace:
+    with open(_file,'w', encoding="utf-8") as replace:
         for line in temp:
-            replace.write(f"{line}")
+            if 'AAP Image' in line:
+                continue
+            replace.write(f"{line}".strip()+'\n')
 
 def getNumber(s,i=0):
     try:
