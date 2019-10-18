f26133bd1c4c70f3a094f1a430f443985c7f5327
coldtaco <36543008+coldtaco@users.noreply.github.com>
2019-10-18 22:11:17 +1100

    Changed to accomadate new input/target

--- Predictive model/LSTM.py
+++ Predictive model/LSTM.py
@@ -4,11 +4,12 @@ import keras
 import pandas as pd
 import numpy as np
 
-x = pickle.load(open('trumpInput.pkl','rb'))
-y = pickle.load(open('trumpTarget.pkl','rb'))
+intake = pickle.load(open('trumpInput.pkl','rb'))
+target = pickle.load(open('trumpTarget.pkl','rb'))
 vocab = len(pickle.load(open('newTrumpDict.pkl','rb'))) + 1
 model = keras.Sequential()
 model.add(keras.layers.Embedding(vocab,512))
+model.add(keras.layers.LSTM(512,return_sequences=True))
 model.add(keras.layers.LSTM(512,return_sequences=True))
 model.add(keras.layers.LSTM(1024))
 model.add(keras.layers.Dropout(0.2))
@@ -18,12 +19,18 @@ earlyStop = keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.001, patie
 rlr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,patience=7, min_lr=1e-6, mode='auto', verbose=1)
 
 def getInpOut(ind):
-    if (ind+1)*500 > len(x):
-        return (x[500*ind:],y[500*ind:])
-    return (x[500*ind:500*(ind+1)],y[500*ind:500*(ind+1)])
-
-for i in range(x//500 +1):
+    print(ind)
+    print(500*ind,500*(ind+1))
+    if (ind+1)*500 > len(intake):
+        return (intake[500*ind:],target[500*ind:])
+    return (intake[500*ind:500*(ind+1)],target[500*ind:500*(ind+1)])
+model.fit(np.array(intake),keras.utils.to_categorical(target,num_classes=vocab),validation_split=0.10, epochs=2000, batch_size=64, verbose=2,callbacks=[earlyStop,rlr])
+model.save('trump.h5')
+exit()
+for i in range(len(intake)//500 +1):
     x,y = getInpOut(i)
+    x = np.array(x)
+    print(len(x),len(y))
     y = keras.utils.to_categorical(y,num_classes=vocab)
     model.fit(x,y,validation_split=0.10, epochs=1000, batch_size=32, verbose=2,callbacks=[earlyStop,rlr])
 model.save('trump.h5')
